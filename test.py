da045d72ef846b568c362e0ebb8ab91e1ee7b6b7
KeGuiChao <kgc912739767@163.com>
2017-12-26 15:23:49 +0800

    add test.py file

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python 
+
+import sys
+
+print 'you entered', len(sys.argv), 'arguments...'
+print 'they were:', str(sys.argv)
+print 'The first argument + third argument is:', 'str(1) + str(3)'
+
